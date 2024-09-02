167d4e842765ba8b7545f1a25b315a7ce818c5bd
ator-dev <dominic.codedeveloper@gmail.com>
2024-09-02 14:05:36 +0100

    Re-license under EUPL-1.2-or-later
    - Change licence from MIT to European Union Public Licence v. 1.2
    - Rename LICENSE.md => LICENCE.md
    - Add EUPL-1.2 licence header to every TypeScript and HTML source file

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,9 @@
+/*
+ * This file is part of Mark My Search.
+ * Copyright © 2021-present ‘ator-dev’, Mark My Search contributors.
+ * Licensed under the EUPL-1.2-or-later.
+ */
+
 if (this.importScripts) {
 	// Required for service workers, whereas event pages use declarative imports.
 	this.importScripts(
